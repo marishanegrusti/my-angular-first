13ca63c0a6b68164d9881f5f6e1101f68535b0a2
marishanegrusti <marishanegrusti>
2024-05-16 12:30:22 +0300

    added interface ITask in maz13_5_2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import {TodoListComponent} from './components/todo-list/todo-list.component';
+//import {ITask} from './components/todo-list/todo-list.type';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, HomeComponent],
+  imports: [RouterOutlet,HeaderComponent, HomeComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { ITask } from './todo-list.type';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  //@Input() tasks: ITask[] = [title: 'Це заголовок1',  discription: 'Це коментар1'];
+  @Input() tasks: ITask[] = [];
+  @Output() taskChanged: EventEmitter<ITask>;
 
+  constructor() { 
+    this.taskChanged = new EventEmitter();
+  }
+  
+  public onTaskClick(changedTask: ITask): void {
+    this.taskChanged.emit(changedTask)
+  }
 }
+
