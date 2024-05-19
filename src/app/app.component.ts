87ef02811c5e60ce70a0d08ee0a46f02ac469c61
marishanegrusti <marishanegrusti>
2024-05-20 01:10:05 +0300

    change *ngFor in maz13_5_2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,28 @@ import { ITask } from './components/todo-list/todo-list.type';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+[x: string]: any;
   title : string = 'my-angular-first-77раз';
   isDisabled=false;//true;
+  //onTaskClick=true;
   // tasks = [{title: 'новий т 17'},{title : 'новий заголовок 22'}];
   //tasks = [{title: 'Test0', description: 'Description0'} , {title: 'Test1', description: 'Description1'}] ;
-  tasks: ITask[] =  [{title: 'Test_1002424', description: 'Description_0024242'} , {title: 'Test_11', description: 'Description_11'}] ;
+  tasks: ITask[] =  [{title: 'Заголовок1_родительский', description: 'Содержание1_род.'} , {title: 'Title_2', description: 'Description_2'}] ;
   // task: ITask= { title: 'Title88_app-root',  description: 'Comentar1'}
+
+  
+  // protected readonly onTaskClick = onTaskClick;
+
+  // onTaskClick($event: string) {
+  //   console.log("eventEmmiter:",$event)
+  // }
+
+
+  // protected readonly onclick = onclick;
+
+  // onClick($event: string) {
+  //   console.log("eventEmmiter:",$event)
+  // }
+
 
 }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter, TrackByFunction} from '@angular/core';
 import { ITask } from './todo-list.type';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
@@ -15,13 +16,57 @@ export class TodoListComponent  {  //implements OnInit
   @Input() tasks: ITask[] = [];
   // @Input() tasks: ITask[] =[{title: 'Test', description: 'Test Description'} , {title: 'Test 1', description: 'Test Description 1'}] 
   @Output() taskChanged: EventEmitter<ITask>;
+// trackByItemId: TrackByFunction<ITask> | undefined;
 
   constructor() { 
     this.taskChanged = new EventEmitter();
   }
   
+
+  // onClick() {
+  //   this.articleChange.emit(this.articleViewer.title)
+  // }
+
   public onTaskClick(changedTask: ITask): void {
+    // this.taskChanged.emit({ title: 'Title88_app-todo-list',  description: 'Comentar1'})
     this.taskChanged.emit(changedTask)
+
   }
 }
 
+//==============
+// *@Component*  
+// ({
+// selector: 'zippy',
+// template: `
+// <div class="zippy">
+// <div (click)="toggle()">Toggle</div>
+// <div [hidden]="!visible">
+// <ng-content></ng-content>
+// </div>
+// </div>`})
+// export class Zippy {
+// visible: boolean = true;  
+
+// *@Output* — () open: EventEmitter<any> = new EventEmitter();  
+
+// *@Output*  
+// () close: EventEmitter<any> = new EventEmitter();
+
+// toggle() {
+// this.visible = !this.visible;
+// if (this.visible) {
+// this.open.emit(null);
+// } else {
+// this.close.emit(null);
+// }
+// }
+// }
+// Access the event object with the $event argument passed to the output event handler:
+
+// <zippy (open)="onOpen($event)" (close)="onClose($event)"></zippy>
+// @see — Observables in Angular
+
+// @publicApi
+
+// @publicApi
